ccfc3b41756bf2ed195ae1fce5c786cefeed4ba0
Garfield Lee <weisiong.lee@gmail.com>
2026-04-19 02:03:18 +0800

    Refactored, utilizing GUIUtils.ts

--- src/ts/uiSkySun.ts
+++ src/ts/uiSkySun.ts
@@ -1,7 +1,10 @@
 // AtmosphereUI.ts
 
 import GUI from 'lil-gui';
+import { GuiController } from './utils/GUIUtils';
+
 import { MovementController } from './utils/MovementController';
+
 
  //: The AtmosphereUI class encapsulates the user interface for 
  //: controlling various atmospheric and camera settings in the sky and sun rendering application.
@@ -12,6 +15,7 @@ export class AtmosphereUI {
     private gui: GUI;
     private settings: any; 
     private movController: MovementController; 
+    private guiCtrl : GuiController;
 
     //: Constructor initializes the UI with references to the GUI instance, settings object, and movement controller.
     constructor(
@@ -20,8 +24,10 @@ export class AtmosphereUI {
         movController: MovementController, 
     ) {
         this.gui = gui;
+        this.guiCtrl = new GuiController(this.gui);
         this.settings = settings; 
         this.movController = movController; 
+
     }
 
     //: This method initializes the UI by creating folders and controls for different categories of settings.
@@ -39,24 +45,6 @@ export class AtmosphereUI {
 
     //--- PRIVATE METHODS
 
-    //: This helper method generates a unique ID for a given controller and assigns it to the corresponding input element in the GUI.
-    private autoId(name: string, controller: any): any {
-
-        const input = controller.domElement.querySelector('input, select, checkbox');
-
-        if (input) {
-            //: Generate a unique ID based on the property name and the provided name, and assign it to the input element. 
-            //: This allows for better accessibility and potential future styling.
-            const id = `gui-${controller._property}-${name.replace(/\s+/g, '-').toLowerCase()}`;
-            input.id = id;
-            input.setAttribute('name', id);
-        }
-
-        //: Return for chaining
-        return controller; 
-    }
-
-
     //: This method sets up the ATMOSPHERE folder and its controls
     private xAddAtmostphereFolder() {
 
@@ -66,23 +54,28 @@ export class AtmosphereUI {
 
         //: The aerosol control is a slider that allows the user to adjust the aerosol density in the atmosphere, 
         //: which affects the scattering and overall appearance of the sky.
-        this.autoId(
-            'aerosol',
-            atm.add(this.settings, 'aerosol', 0.1, 30.0, 0.1)
-                .decimals(1)
-                .name(' ▪ aerosol')
-                .listen()
-        );
+ 
+        this.guiCtrl.addControl(atm, this.settings, 'aerosol', {
+            label: ' ▪ aerosol',
+            min: 0.1,
+            max: 30.0,
+            step: 0.1,
+            decimals: 1,
+            listen: true
+        });
 
         atm.add(this.settings, 'enableDust').name(' ▪ dust');
 
         //: The wind intensity control is a slider that adjusts the strength of the wind effect in the atmosphere,
-        this.autoId(
-            'windintensity',
-            atm.add(this.settings, 'windIntensity', .0, 1.0, 0.02)
-                .decimals(2)
-                .name(' ▪ wind')
-        );
+ 
+        this.guiCtrl.addControl(atm, this.settings, 'windIntensity', {
+            label: ' ▪ wind',
+            min: 0.,
+            max: 1.0,
+            step: 0.02,
+            decimals: 2,
+            listen: false
+        });
 
         return;
     }
@@ -96,14 +89,17 @@ export class AtmosphereUI {
         sun.add(this.settings, 'enableHeatHaze').name(' ▪ heat haze');
         sun.add(this.settings, 'enableLimbDarken').name(' ▪ limb dark');
 
-        //: The sun elevation control is a slider that allows the user to adjust the angle of the sun above the horizon, which affects the lighting and color of the sky.
-        this.autoId( 'sunelev',
-            sun.add(this.settings, 'sunElevation', -20, 89.0,0.1)
-                .decimals(1)
-                .name(' ▪ elev (deg)')
-                .listen()
-        );
+        //: The sun elevation control is a slider that allows the user to adjust the angle of the sun above the horizon, 
+        //: which affects the lighting and color of the sky.
 
+        this.guiCtrl.addControl(sun, this.settings, 'sunElevation', {
+            label: ' ▪ elev (deg)',
+            min: -20,
+            max: 89.0,
+            step: 0.1,
+            decimals: 1,
+            listen: true
+        });
   
         return;
     }
@@ -116,23 +112,32 @@ export class AtmosphereUI {
         lens.add(this.settings, 'enableFlare').name(' ▪ flare');
         lens.add(this.settings, 'enableLensDirt').name(' ▪ dirt');
 
-        this.autoId('lensdirtweight',
-            lens.add(this.settings, 'lensDirtWeight', 0, 2, 0.1)
-                .decimals(1)
-                .name(' ▪ dirt wgt.')
-        );
+        this.guiCtrl.addControl(lens, this.settings, 'lensDirtWeight', {
+            label: ' ▪ dirt wgt.',
+            min: 0,
+            max: 2,
+            step: 0.1,
+            decimals: 1,
+            listen: false
+        });
 
-        this.autoId('lensdirtstep0',
-            lens.add(this.settings, 'lensDirtStep0', 0, 1, 0.1)
-                .decimals(1)
-                .name(' ▪ dirt step0')
-        );
-        
-        this.autoId('lensdirtstep1',
-            lens.add(this.settings, 'lensDirtStep1', 0, 5, 0.1)
-                .decimals(1)
-                .name(' ▪ dirt step1')
-        );
+        this.guiCtrl.addControl(lens, this.settings, 'lensDirtStep0', {
+            label: ' ▪ dirt step0',
+            min: 0,
+            max: 1,
+            step: 0.1,
+            decimals: 1,
+            listen: false
+        });
+
+        this.guiCtrl.addControl(lens, this.settings, 'lensDirtStep1', {
+            label: ' ▪ dirt step1',
+            min: 0,
+            max: 5,
+            step: 0.1,
+            decimals: 1,
+            listen: false
+        });
 
         return;
     }
@@ -160,56 +165,65 @@ export class AtmosphereUI {
         this.addRotationSlider(cam, 'camPitch', 'pitch');
         this.addRotationSlider(cam, 'camYaw', 'yaw');
 
-        this.autoId('camfov',
-            cam.add(this.settings, 'camFov', 5, 170,0.5).name(' ▪ fOV')
-            .decimals(1)
-            .listen()
-            .onChange( (val: number) => {
+        this.guiCtrl.addControl(cam, this.settings, 'camFov', {
+            label: ' ▪ fOV',
+            min: 5,
+            max: 170,
+            step: 0.5,
+            decimals: 1,
+            listen: true,
+            onChange: (val: number) => {
                 this.movController.camFOV = val;
-            })
-        );
+            }
+        });
 
-        this.autoId('eyeattitude',
-        cam.add(this.settings, 'eyeAttitude', 0.02, 80, 0.1)
-            .decimals(2)
-            .name(' ▪ altitude')
-            .listen()
-            .onChange( (val: number) => {
+        this.guiCtrl.addControl(cam, this.settings, 'eyeAttitude', {
+            label: ' ▪ altitude',
+            min: 0.02,
+            max: 80,
+            step: 0.1,
+            decimals: 2,
+            listen: true,
+            onChange: (val: number) => {
                 this.movController.camYPos = val;
-            })
-        );
+            }
+        });
         return;
     }
 
     //: This method adds a rotation slider for camera control and sets up its onChange behavior
     private addRotationSlider(folder: any, prop: string, label: string) {
 
-        return this.autoId(prop.toLowerCase(), 
-            folder.add(this.settings, prop, -180, 180, 0.1)
-                .decimals(1)
-                .name(` ▪ ${label}`)
-                .listen()
-                .onChange((val: number) => {
+        this.guiCtrl.addControl(folder, this.settings, prop, {
+            label: ` ▪ ${label}`,
+            min: -180,
+            max: 180,
+            step: 0.1,
+            decimals: 1,
+            listen: true,
+            onChange: (val: number) => {
 
-                    // 1. Calculate the delta from the LAST KNOWN STATE (not the current slider value, which may have wrapped around)
-                    const stateKey = `_prev${prop.replace('cam', '')}` as keyof typeof this.settings;
-                    
-                    let delta = val - (this.settings[stateKey] as number);
+                //: 1. Calculate the delta from the LAST KNOWN STATE (not the current slider value, 
+                //: which may have wrapped around)
+                const stateKey = `_prev${prop.replace('cam', '')}` as keyof typeof this.settings;
+                
+                let delta = val - (this.settings[stateKey] as number);
 
-                    // 2. Handle the -180/180 wrap-around for the slider
-                    if (delta > 180) delta -= 360;
-                    if (delta < -180) delta += 360;
+                //: 2. Handle the -180/180 wrap-around for the slider
+                if (delta > 180) delta -= 360;
+                if (delta < -180) delta += 360;
 
-                    // 3. Convert delta to radians for the movement controller
-                    const rad = delta * Math.PI / 180;
-                    const euler = this.movController.update(
-                        prop === 'camPitch' ? -rad : 0,
-                        prop === 'camYaw' ? rad : 0,
-                        prop === 'camRoll' ? rad : 0
-                    );
-                    this.syncRotation(euler); // Atomic update
-                })
-        );
+                //: 3. Convert delta to radians for the movement controller
+                const rad = delta * Math.PI / 180;
+                const euler = this.movController.update(
+                    prop === 'camPitch' ? -rad : 0,
+                    prop === 'camYaw' ? rad : 0,
+                    prop === 'camRoll' ? rad : 0
+                );
+                this.syncRotation(euler); // Atomic update
+            }
+        });
+
         return;
     }
 
@@ -220,11 +234,14 @@ export class AtmosphereUI {
         pp.add(this.settings, 'enableDither').name(' ▪ dither');
         pp.add(this.settings, 'enableGrain').name(' ▪ grain');
 
-        this.autoId('grainweight',
-            pp.add(this.settings, 'grainWeight', 0, 3, 0.1)
-            .name(' ▪ grain wgt.')
-            .decimals(1)
-        );
+        this.guiCtrl.addControl(pp, this.settings, 'grainWeight', {
+            label: ' ▪ grain wgt.',
+            min: 0,
+            max: 3,
+            step: 0.1,
+            decimals: 1,
+            listen: false
+        });
 
         return;
     }
@@ -243,12 +260,15 @@ export class AtmosphereUI {
         const debug = this.gui.addFolder('DEBUG');
 
         debug.add(this.settings, 'enableCheckerboard').name(' ▪ checkerboard');
-        
-        this.autoId('checkerboardscale',
-            debug.add(this.settings, 'checkerboardScale', 1.0, 200.0, 1)
-            .decimals(0)
-            .name(' ▪ scale')
-        );
+
+        this.guiCtrl.addControl(debug, this.settings, 'checkerboardScale', {
+            label: ' ▪ scale',
+            min: 1.0,
+            max: 200.0,
+            step: 1,
+            decimals: 0,
+            listen: false
+        });
  
         return;
     }
